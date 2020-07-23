1dd3ff8d45cf5a473efeb440d3288c242e61c4dd
Thomas <tg.59290@gmail.com>
2020-07-23 14:28:03 +0200

    add base html and css for questions with collapse

---
+++ js/questions.js
@@ -0,0 +1,13 @@
+// let questions = document.getElementsByClassName("question");
+//
+// for(let question of questions) {
+//   question.addEventListener("click", function() {
+//     alert("coucou");
+//   })
+// }
+
+// let answers = document.getElementsByClassName("answer");
+//
+// for(answer of answers) {
+//   answer.style.display = "none";
+// }
