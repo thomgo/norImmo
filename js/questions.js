9d648783bb090a03daf6a7ca831978d084fa396e
Thomas <tg.59290@gmail.com>
2020-07-23 15:21:33 +0200

    first draft of questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,13 +1,20 @@
-// let questions = document.getElementsByClassName("question");
-//
-// for(let question of questions) {
-//   question.addEventListener("click", function() {
-//     alert("coucou");
-//   })
-// }
+let questionBlocs = document.getElementsByClassName("questionAnswer");
 
-// let answers = document.getElementsByClassName("answer");
-//
-// for(answer of answers) {
-//   answer.style.display = "none";
-// }
+for(let bloc of questionBlocs) {
+  bloc.addEventListener("click", function() {
+    let answer = this.querySelector(".answer");
+    let text = answer.querySelector("p");
+    let arrow = this.querySelector(".fa");
+    if(!answer.classList.contains("deployed")) {
+      answer.style.height = `${text.offsetHeight}px`;
+      arrow.classList.remove("fa-arrow-down");
+      arrow.classList.add("fa-arrow-up");
+    }
+    else {
+      answer.style.height = "0px";
+      arrow.classList.remove("fa-arrow-up");
+      arrow.classList.add("fa-arrow-down");
+    }
+    answer.classList.toggle("deployed");
+  })
+}
